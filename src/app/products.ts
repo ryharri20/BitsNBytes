79c6e591e01f36d88e117d5cf12793c096c53a55
ryharri20 <ryharri@okstate.edu>
2020-05-04 22:17:09 -0500

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -4,42 +4,42 @@ export const products = [
     name: 'Asus VivoBook S15',
     price: 749,
     description: '',
-    imageUrl: "assets/images/AsusVivoBook.jpg"  
+    imageUrl: "https://raw.githubusercontent.com/ryharri20/BitsNBytes/master/src/assets/images/AsusVivoBook.jpg"  
   },
   {
     ID: 2,
     name: 'Acer Spin 5',
     price: 699,
     description: '',
-    imageUrl: "assets/images/AcerSpin5.jpg"  
+    imageUrl: "https://raw.githubusercontent.com/ryharri20/BitsNBytes/master/src/assets/images/AcerSpin5.jpg"  
   },
   {
     ID: 3,
     name: 'HP Grade A Laptop ProBook 640',
     price: 399,
     description: '',
-    imageUrl: "assets/images/HPGradeA.jpg"  
+    imageUrl: "https://raw.githubusercontent.com/ryharri20/BitsNBytes/master/src/assets/images/HPGradeA.jpg"  
   },
   {
     ID: 4,
     name: 'ASUS ZenBook Pro Duo ',
     price: 2500,
     description: '',
-    imageUrl: "assets/images/AsusZenbook.jpg"  
+    imageUrl: "https://raw.githubusercontent.com/ryharri20/BitsNBytes/master/src/assets/images/AsusZenbook.jpg"  
   },
   {
     ID: 5,
     name: 'Lenovo Laptop 100e',
     price: 299,
     description: '',
-    imageUrl: "assets/images/Lenovo.jpg"  
+    imageUrl: "https://raw.githubusercontent.com/ryharri20/BitsNBytes/master/src/assets/images/Lenovo.jpg"  
   },
   {
     ID: 6,
     name: 'MSI GS Series GS75',
     price: 2600,
     description: '',
-    imageUrl: "assets/images/MSI.jpg"  
+    imageUrl: "https://raw.githubusercontent.com/ryharri20/BitsNBytes/master/src/assets/images/MSI.jpg"  
   },
 ];
 
